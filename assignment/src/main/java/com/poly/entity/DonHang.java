c0068256cd48a0193bd32f3667324d04d5b5da17
anhthai98 <anhthai057@gmail.com>
2018-06-12 23:36:17 +0700

    thaiver1.1

--- assignment/src/main/java/com/poly/entity/DonHang.java
+++ assignment/src/main/java/com/poly/entity/DonHang.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -14,7 +15,7 @@ import javax.persistence.OneToOne;
 public class DonHang {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int maDonHang;
 	private String tenMatHang;
 	private String tenNguoiNhan;
---
+++ assignment/src/main/java/com/poly/service/DonHangService.java
@@ -0,0 +1,43 @@
+package com.poly.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.poly.entity.DonHang;
+import com.poly.entity.KhachHang;
+import com.poly.impl.DonHangImpl;
+import com.poly.model.DonHangDAO;
+
+@Service
+public class DonHangService implements DonHangImpl {
+	
+	@Autowired
+	DonHangDAO donhangDAO;
+
+	@Override
+	public boolean taoDonHang(DonHang donhang) {
+		boolean ck = donhangDAO.taoDonHang(donhang);
+		return ck;
+	}
+
+	@Override
+	public boolean suaDonHang(DonHang donhang) {
+		boolean ck = donhangDAO.suaDonHang(donhang);
+		return ck;
+	}
+
+	@Override
+	public List<DonHang> danhsachDonHang() {
+		List<DonHang> lst = donhangDAO.danhsachDonHang();
+		return lst;
+	}
+
+	@Override
+	public List<KhachHang> danhsachKhachHang() {
+		List<KhachHang> lst = donhangDAO.danhsachKhachHang();
+		return lst;
+	}
+
+}
