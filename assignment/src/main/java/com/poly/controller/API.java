55cb58c5215c1095a0ee2cebe27788fd7707159a
minhddpd01958 <minhddpd01958@fpt.edu.vn>
2018-06-24 01:37:45 +0700

    minh v4.5
    fix css, fix trang khách hàng

--- assignment/src/main/java/com/poly/controller/API.java
+++ assignment/src/main/java/com/poly/controller/API.java
@@ -18,7 +18,6 @@ import com.poly.entity.DonHang;
 import com.poly.entity.JSONChucVu;
 import com.poly.entity.JSONDonHang;
 import com.poly.entity.JSONNhanVien;
-import com.poly.entity.KhachHang;
 import com.poly.entity.NhanVien;
 import com.poly.entity.TinhTrangDonHang;
 import com.poly.service.ChucVuService;
@@ -130,13 +129,6 @@ public class API {
 	@ResponseBody
 	public List<TinhTrangDonHang> dsTinhTrangDonHang() {
 		List<TinhTrangDonHang> list = tinhTrangDonHangService.danhsachTinhTrang();
-		return list;
-	}
-	
-	@GetMapping(path = "/thongtinkh/{maKH}", produces = "application/json; charset=utf-8")
-	@ResponseBody
-	public List<DiaChiKhachHang> thongTinKHBoiMaKH(@PathVariable("maKH") int maKH) {
-		List<DiaChiKhachHang> list = diaChiKhachHangService.layDSDiaChiTheoMaKH(maKH);
 		return list;
 	}
 }
